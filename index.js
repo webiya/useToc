15f34ca441e0582689a41a296a69e3062812c660
Oryam <48219965+or-yam@users.noreply.github.com>
2021-06-27 07:22:16 +0300

    Add ability to change calssLists

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 import { useEffect, useState } from 'react';
 
-export default function (htmlSelector) {
+const updateClassLists = (list, method) => {
+  list.forEach(({ selector, className }) => {
+    const element = document.querySelector(selector)?.classList;
+    if (!element) return;
+
+    if (method === 'add') {
+      element.add(className);
+    }
+
+    if (method === 'remove') {
+      element.remove(className);
+    }
+  });
+};
+
+export default function (selector, effectDependencies = [], additionalClassList = []) {
   const [tocData, setTocData] = useState([]);
 
   useEffect(() => {
-    const templateContext = [...document.querySelectorAll(htmlSelector)].map(({ innerText, id }) => ({
+    const templateContext = [...document.querySelectorAll(selector)].map(({ innerText, id }) => ({
       text: innerText,
       id
     }));
     setTocData(templateContext);
+    updateClassLists(additionalClassList, 'add');
     return () => {
       setTocData('');
+      updateClassLists(additionalClassList, 'remove');
     };
-  }, []);
+  }, [...effectDependencies]);
+
   return tocData;
 }
